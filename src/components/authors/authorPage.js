b310e79a88c7db1cc6946d53f2b90cce8b9765df
Doug Coleman <douglas.coleman@daugherty.com>
2018-07-22 19:28:22 -0500

    Completed the course

--- src/components/authors/authorPage.js
+++ src/components/authors/authorPage.js
@@ -2,21 +2,29 @@
 
 var React = require('react');
 var Router = require('react-router');
-var Link = Router.Link;
-var AuthorApi = require('../../api/authorApi');
+var Link = require('react-router').Link;
+var AuthorActions = require('../../actions/authorActions');
+var AuthorStore = require('../../stores/authorStore');
 var AuthorList = require('./authorList');
 
 var AuthorPage = React.createClass({
 
     getInitialState: function() {
         return {
-            authors: []
+            authors: AuthorStore.getAllAuthors()
         };
     }, 
-    componentDidMount: function() {
-        if (this.isMounted()) {
-            this.setState({authors: AuthorApi.getAllAuthors()});
-        }
+
+    componentWillMount: function() {
+        AuthorStore.addChangeListener(this._onChange);
+    },
+
+    componentWillUnmount: function() {
+        AuthorStore.removeChangeListener(this._onChange);
+    },
+
+    _onChange: function() {
+        this.setState({authors: AuthorStore.getAllAuthors() });
     },
 
     render: function () {
@@ -24,7 +32,7 @@ var AuthorPage = React.createClass({
         return (
             <div>
                 <h1>Authors</h1>
-                <Link to="addAuthors" className="btn btn-default">Add Author</Link>
+                <Link to="addAuthor" className="btn btn-default">Add Author</Link>
                 <AuthorList authors={this.state.authors} />
             </div>
 
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 "use strict";
 
 var React = require('react');
+
 var Router = require('react-router');
 var DefaultRoute = Router.DefaultRoute;
 var Route = Router.Route;
@@ -11,9 +12,8 @@ var routes = (
     <Route name="app" path="/" handler={require('./components/app')}>
         <DefaultRoute handler={require('./components/homePage')} />
         <Route name="authors" handler={require('./components/authors/authorPage')} />
-        <Route name="addAuthors" 
-            path="author" 
-            handler={require('./components/authors/manageAuthorPage')} />
+        <Route name="addAuthor" path="author" handler={require('./components/authors/manageAuthorPage')} />
+        <Route name="manageAuthors" path="author/:id" handler={require('./components/authors/manageAuthorPage')} />
         <Route name="about" handler={require('./components/about/aboutPage')} />
         <NotFoundRoute handler={require('./components/notFoundPage')} />
         <Redirect from="about-us" to="about" />
